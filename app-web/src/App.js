5f4df433dab96a0fc0bc321ab6f69ad2b42eb70d
aeduardogonzalez <aeduardogonzalez@unicesar.edu.co>
2024-05-18 17:56:24 -0500

    Modificación de componentes y páginas

--- app-web/src/App.js
+++ app-web/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
--- app-web/src/Paginas/AcercaDe.jsx
+++ app-web/src/Paginas/AcercaDe.jsx
@@ -1,41 +1,41 @@
 import { useAuth } from "../Componentes/AuthProvider";
-import NavBar from "../Componentes/NavBar";
+import {Link} from 'react-router-dom';
 import Bambu from "../Imagenes/Bambu.png";
 import Mirador from "../Imagenes/Conjunto.png";
-import "../Css/Dashboard.css"
-import {Link} from 'react-router-dom';
+import Margaritas from "../Imagenes/LasMargaritas.png";
+import MiradorSierra from "../Imagenes/MiradorDeLaSierra.png";
+import "../Css/AcercaDe.css"
 
 const Espacios = () => {
     const auth = useAuth();
     return (
-        <nav>
-            <ul>
-                <li className='link1'>
-                    <Link className="direccion" to="/">{auth.user}</Link>
-                </li>
+        <>
+            <nav>
+                <ul>
+                    <li className='link1'>
+                        <Link className="direccion" to="/">{auth.user}</Link>
+                    </li>
 
-                <li className='l_concernet'>
-                    <Link className="direccion" to="/">ConcerNet</Link>
-                </li>
+                    <li className='l_concernet'>
+                        <Link className="direccion" to="/">ConcerNet</Link>
+                    </li>
 
-                <li className='link'>
-                    <Link onClick={() => auth.logOut()} className="direccion" > Salir</Link>
-                </li>
+                    <div id="Nav_AcercaDe">
+                        <li className='link2'>
+                            <Link onClick={() => auth.logOut()} className="direccion" > Salir</Link>
+                        </li>
 
-                <li className='link'>
-                    <Link className="direccion" to="/AcercaDe">Acerca De</Link>
-                </li>
+                        <li className='link3'>
+                            <Link className="direccion" to="/Dashboard">Volver</Link>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
 
-                <li className='link'>
-                    <Link className="direccion" to="/Espacios">Espacios</Link>
-                </li>
-
-                <li className='link'>
-                    <Link className="direccion" to="/Dashboard">Inicio</Link>
-                </li>
-
-            </ul>
-        </nav>
+            <div className="cuadro_imgs">
+                <img src={Bambu} className="img_Bambu" alt="Bambu" />
+            </div>
+        </>
     );
 };
 
