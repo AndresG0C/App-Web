76ae2c4da9d75798b4505390d5af4436199e2d47
aeduardogonzalez <aeduardogonzalez@unicesar.edu.co>
2024-05-18 18:49:21 -0500

    Acerca De Terminado

--- app-web/src/Paginas/AcercaDe.jsx
+++ app-web/src/Paginas/AcercaDe.jsx
@@ -26,14 +26,27 @@ const Espacios = () => {
                         </li>
 
                         <li className='link3'>
-                            <Link className="direccion" to="/Dashboard">Volver</Link>
+                            <Link className="direccion" to="/Dashboard">Regresar</Link>
                         </li>
                     </div>
                 </ul>
             </nav>
+            <div className="container">
+                <div className="contenedor_imagenes">                
+                    <img src={Bambu} className="a_img_Bambu" alt="Bambu" />
+                    <img src={Mirador} className="a_img_Mirador" alt="Mirador" />
+                    <img src={Margaritas} className="a_img_Margaritas" alt="Margaritas" />
+                    <img src={MiradorSierra} className="a_img_MiradorSierra" alt="Mirador Sierra" />
 
-            <div className="cuadro_imgs">
-                <img src={Bambu} className="img_Bambu" alt="Bambu" />
+                    <div className="cuadro_texto">
+                        <p className="text_about">
+                            Nuestro principal objetivo es facilitar las tareas de gestion del conjunto cerrado, como
+                            el pago de administración, el alquiler de espacios comunes, además de informarse de los
+                            horarios y actividades que se realizan en el conjunto, logrando así una mejor convivencia
+                            entre los habitantes, generando un mayor orden y organización dentro del conjunto cerrado.
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     );
--- app-web/src/Paginas/Espacios.jsx
+++ app-web/src/Paginas/Espacios.jsx
@@ -8,34 +8,30 @@ import {Link} from 'react-router-dom';
 const Espacios = () => {
     const auth = useAuth();
     return (
-        <nav>
-            <ul>
-                <li className='link1'>
-                    <Link className="direccion" to="/">{auth.user}</Link>
-                </li>
+        <>
+            <nav>
+                <ul>
+                    <li className='link1'>
+                        <Link className="direccion" to="/">{auth.user}</Link>
+                    </li>
 
-                <li className='l_concernet'>
-                    <Link className="direccion" to="/">ConcerNet</Link>
-                </li>
+                    <li className='l_concernet'>
+                        <Link className="direccion" to="/">ConcerNet</Link>
+                    </li>
 
-                <li className='link'>
-                    <Link onClick={() => auth.logOut()} className="direccion" > Salir</Link>
-                </li>
+                    <li className='link'>
+                        <Link onClick={() => auth.logOut()} className="direccion" > Salir</Link>
+                    </li>
 
-                <li className='link'>
-                    <Link className="direccion" to="/AcercaDe">Acerca De</Link>
-                </li>
+                    <li className='link'>
+                        <Link className="direccion" to="/Dashboard">Regresar</Link>
+                    </li>
+                </ul>
+            </nav>
 
-                <li className='link'>
-                    <Link className="direccion" to="/Espacios">Espacios</Link>
-                </li>
+            <div className="container"></div>
 
-                <li className='link'>
-                    <Link className="direccion" to="/Dashboard">Inicio</Link>
-                </li>
-
-            </ul>
-        </nav>
+        </>
     );
 };
 
